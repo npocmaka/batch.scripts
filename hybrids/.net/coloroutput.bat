e0bab95346b7610cfaa45be9d54d6dfcbbe4ea12
Vasil Arnaudov <npocmaka@users.noreply.github.com>
2014-06-18 23:54:30 +0300

    Update coloroutput.bat

--- hybrids/.net/coloroutput.bat
+++ hybrids/.net/coloroutput.bat
@@ -25,8 +25,35 @@ var newLine = false;
 var output = "";
 var foregroundColor = 0;
 var backgroundColor = 14;
+var evaluate = false;
 var currentBackground=Console.BackgroundColor;
 var currentForeground=Console.ForegroundColor;
+
+
+//http://stackoverflow.com/a/24294348/388389
+var jsEscapes = {
+  'n': '\n',
+  'r': '\r',
+  't': '\t',
+  'f': '\f',
+  'v': '\v',
+  'b': '\b'
+};
+
+function decodeJsEscape(_, hex0, hex1, octal, other) {
+  var hex = hex0 || hex1;
+  if (hex) { return String.fromCharCode(parseInt(hex, 16)); }
+  if (octal) { return String.fromCharCode(parseInt(octal, 8)); }
+  return jsEscapes[other] || other;
+}
+
+function decodeJsString(s) {
+  return s.replace(
+      // Matches an escape sequence with UTF-16 in group 1, single byte hex in group 2,
+      // octal in group 3, and arbitrary other single-character escapes in group 4.
+      /\\(?:u([0-9A-Fa-f]{4})|x([0-9A-Fa-f]{2})|([0-3][0-7]{0,2}|[4-7][0-7]?)|(.))/g,
+      decodeJsEscape);
+}
 
 function colorSetter( i:Int32 ):ConsoleColor {
 	switch ( i ) {
@@ -86,22 +113,28 @@ function colorSetter( i:Int32 ):ConsoleColor {
 
 
 function printHelp( ) {
-   print( arguments[0] + "-s string [-f foreground] [-b background] [-n]" );
+   print( arguments[0] + "  -s string [-f foreground] [-b background] [-n]" );
    print( " " );
    print( "	string 			String to be printed" );
-   print( "	foreground 		Foreground color - a number between 0 and 15.Default is 0" );
-   print( "	background 		background color - a number between 0 and 15.Default is 14" );
-   print( "	-n 				Indicates if a new line should be written at the end of the string(by default - no)." );
+   print( "	foreground 		Foreground color - a " );
+   print( "					number between 0 and 15.Default is 0" );
+   print( "	background 		background color - a " );
+   print( "					number between 0 and 15.Default is 14" );
+   print( "	-n 		        Indicates if a new line should" );
+   print( "					be written at the end of the ");
+   print( "					string(by default - no)." );
    print( "" );
    print( "Colors :" );
    for ( var c = 0 ; c < 16 ; c++ ) {
-		Console.Write( ""+c+"-" );
+		
 		Console.BackgroundColor = colorSetter(c);
 		Console.Write( " " );
 		Console.BackgroundColor=currentBackground;
+		Console.Write( "-"+c );
 		Console.WriteLine( "" );
    }
    Console.BackgroundColor=currentBackground;
+   
    
 
 }
@@ -110,6 +143,9 @@ function errorChecker( e:Error ) {
 		if ( e.message == "Input string was not in a correct format." ) {
 			print( "the color parameters should be numbers between 0 and 15" );
 			Environment.Exit( 1 );
+		} else if (e.message == "Index was outside the bounds of the array.") {
+			print( "invalid arguments" );
+			Environment.Exit( 2 );
 		} else {
 			print ( "Error Message: " + e.message );
 			print ( "Error Code: " + ( e.number & 0xFFFF ) );
@@ -134,6 +170,10 @@ if ( arguments.length == 1 || arguments[1].toLowerCase() == "-help" || arguments
 for (var arg = 1; arg <= arguments.length-1; arg++ ) {
 	if ( arguments[arg].toLowerCase() == "-n" ) {
 		newLine=true;
+	}
+	
+	if ( arguments[arg].toLowerCase() == "-e" ) {
+		evaluate=true;
 	}
 	
 	if ( arguments[arg].toLowerCase() == "-s" ) {
@@ -161,14 +201,20 @@ for (var arg = 1; arg <= arguments.length-1; arg++ ) {
 
 
 
-Console.BackgroundColor=colorSetter( backgroundColor );
-Console.ForegroundColor=colorSetter( foregroundColor );
-if ( newLine ) {
-	Console.WriteLine(output);
-} else {
-	Console.Write(output);
+Console.BackgroundColor = colorSetter( backgroundColor );
+Console.ForegroundColor = colorSetter( foregroundColor );
+
+if ( evaluate ) {
+	output=decodeJsString(output);
 }
 
-Console.BackgroundColor=currentBackground;
-Console.ForegroundColor=currentForeground;
+if ( newLine ) {
+	Console.WriteLine(output);	
+} else {
+	Console.Write(output);
+	
+}
+
+Console.BackgroundColor = currentBackground;
+Console.ForegroundColor = currentForeground;
 
