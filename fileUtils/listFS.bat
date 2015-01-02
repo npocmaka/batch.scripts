99205499b59cdd44e26080ad7a061e650661815b
Vasil Arnaudov <npocmaka@users.noreply.github.com>
2015-01-02 16:35:38 +0200

    Create listFS.bat

---
+++ fileUtils/listFS.bat
@@ -0,0 +1,5 @@
+fltmc volumes | find ":"
+rem :: lists file system types (NTFS,FAT32,CDFS ..) on all drivres
+rem as fltmc utility is not listed in technet A-Z reference it's rather an example how can it be used
+rem requires less code than fsutil
+rem http://stackoverflow.com/questions/6995813/how-can-i-use-batch-to-determine-if-a-computer-is-using-fat32-or-ntfs
