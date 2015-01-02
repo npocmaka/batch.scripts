6562e191ac806f721c9b8ccdc9f9e5782141ed60
Vasil Arnaudov <npocmaka@users.noreply.github.com>
2015-01-02 17:03:53 +0200

    Update firstLines.bat

--- fileUtils/firstLines.bat
+++ fileUtils/firstLines.bat
@@ -1,10 +1,6 @@
-@echo off
 
-set "file_to_process=E:\somefile.txt"
 
-set "first_n_lines=5"
-
-break>"%temp%\empty"&&fc "%temp%\empty" "%file_to_process%" /lb  %first_n_lines% /t |more +4 | findstr /B /E /V "*****"
+break>"%temp%\empty"&&fc "%temp%\empty" "%file_to_process%" /lb  X /t |more +4 | findstr /B /E /V "*****"
 
 rem comperatively brief way to show first N lines of a file
 rem will fail the file contains ***** and MORE command will replace tab characters (if any) with space
