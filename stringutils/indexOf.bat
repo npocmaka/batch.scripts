9b44b402ab6adf47e6f9ae29dcbd83da7270244a
Vasil Arnaudov <npocmaka@users.noreply.github.com>
2015-01-21 20:33:43 +0200

    Create indexOf.bat

---
+++ stringutils/indexOf.bat
@@ -0,0 +1,27 @@
+:indexof [%1 - string ; %2 - find index of ; %3 - if defined will store the result in variable with same name]
+@echo off
+setlocal enableDelayedExpansion
+
+set "str=%~1"
+set "s=!str:%~2=&rem.!"
+set s=#%s%
+if "%s%" equ "#%~1" endlocal& if "%~3" neq "" (set %~3=-1&exit /b 0) else (echo -1&exit /b 0) 
+
+  set "len=0"
+  for %%A in (2187 729 243 81 27 9 3 1) do (
+	set /A mod=2*%%A
+	for %%Z in (!mod!) do (
+		if "!s:~%%Z,1!" neq "" (
+			set /a "len+=%%Z"
+			set "s=!s:~%%Z!"
+			
+		) else (
+			if "!s:~%%A,1!" neq "" (
+				set /a "len+=%%A"
+				set "s=!s:~%%A!"
+			)
+		)
+	)
+  )
+  endlocal & if "%~3" neq "" (set %~3=%len%) else echo %len%
+exit /b 0
