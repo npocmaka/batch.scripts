0a5d257d45f4f78e982e3663d567167424f8b8f1
Vasil Arnaudov <npocmaka@users.noreply.github.com>
2015-01-21 20:36:47 +0200

    Update lastIndexOf.bat

--- stringutils/lastIndexOf.bat
+++ stringutils/lastIndexOf.bat
@@ -1,4 +1,5 @@
 :lastindexof [%1 - string ; %2 - find last index of ; %3 - if defined will store the result in variable with same name]
+::http://ss64.org/viewtopic.php?id=1687
 @echo off
 setlocal enableDelayedExpansion 
 
