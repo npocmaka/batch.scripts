a13c3fd754a726058af132d1b9d9ef1774e5df1f
Vasil Arnaudov <npocmaka@users.noreply.github.com>
2015-05-03 02:20:38 +0300

    Create FirmwareType.bat

---
+++ discovers_and_bugs/discovers/FirmwareType.bat
@@ -0,0 +1,10 @@
+@echo off
+
+:: works on windows 8/2012 or above
+
+echo %FIRMWARE_TYPE% 
+exit /b 0
+:: ===
+Undocumented environment variable .Found here:
+http://www.dostips.com/forum/viewtopic.php?f=3&t=6137
+:: ===
