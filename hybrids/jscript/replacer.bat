c0ff80ea0e9207691969abfff35bc3198097b9ee
Vasil Arnaudov <npocmaka@users.noreply.github.com>
2015-07-24 12:26:16 +0300

    Update replacer.bat

--- hybrids/jscript/replacer.bat
+++ hybrids/jscript/replacer.bat
@@ -9,6 +9,8 @@ exit /b %errorlevel% */0;
 	
 	if (ARGS.Item(0).toLowerCase() == "-help") {
 		WScript.Echo(Wscript.ScriptName + " path_to_file search replace [search replace[search replace [...]]]");
+		WScript.Echo(Wscript.ScriptName + " e?path_to_file search replace [search replace[search replace [...]]]");
+		WScript.Echo("if filename starts with \"e?\" search and replace string will be evaluated for special characters ")
 		WScript.Quit(0);
 	}
 
@@ -21,10 +23,40 @@ exit /b %errorlevel% */0;
 		WScript.Echo("Wrong arguments");
 		WScript.Quit(2);
 	}
+	
+	var jsEscapes = {
+	  'n': '\n',
+	  'r': '\r',
+	  't': '\t',
+	  'f': '\f',
+	  'v': '\v',
+	  'b': '\b'
+	};
+	
+	
+	//string evaluation
+	//http://stackoverflow.com/questions/24294265/how-to-re-enable-special-character-sequneces-in-javascript
+	
+	function decodeJsEscape(_, hex0, hex1, octal, other) {
+	  var hex = hex0 || hex1;
+	  if (hex) { return String.fromCharCode(parseInt(hex, 16)); }
+	  if (octal) { return String.fromCharCode(parseInt(octal, 8)); }
+	  return jsEscapes[other] || other;
+	}
+
+	function decodeJsString(s) {
+	  return s.replace(
+		  // Matches an escape sequence with UTF-16 in group 1, single byte hex in group 2,
+		  // octal in group 3, and arbitrary other single-character escapes in group 4.
+		  /\\(?:u([0-9A-Fa-f]{4})|x([0-9A-Fa-f]{2})|([0-3][0-7]{0,2}|[4-7][0-7]?)|(.))/g,
+		  decodeJsEscape);
+	}
 
 	function replaceAll(find, replace, str) {
 	  return str.replace(new RegExp(find, 'g'), replace);
 	}
+	
+	
 	
   function getContent(file) {
 		// :: http://www.dostips.com/forum/viewtopic.php?f=3&t=3855&start=15&p=28898  ::
@@ -67,8 +99,20 @@ exit /b %errorlevel% */0;
 		ado.Close();	
    }
    
+	if (typeof String.prototype.startsWith != 'function') {
+	  // see below for better implementation!
+	  String.prototype.startsWith = function (str){
+		return this.indexOf(str) === 0;
+	  };
+	}
    
+   
+	var evaluate=false;
     var filename=ARGS.Item(0);
+	if(filename.toLowerCase().startsWith("e?")) {
+		filename=filename.substring(2,filename.length);
+		evaluate=true;
+	}
     var content=getContent(filename);
 	var newContent=content;
 	var find="";
@@ -77,6 +121,10 @@ exit /b %errorlevel% */0;
 	for (var i=1;i<ARGS.Length-1;i=i+2){
 		find=ARGS.Item(i);
 		replace=ARGS.Item(i+1);
+		if(evaluate){
+			find=decodeJsString(find);
+			replace=decodeJsString(replace);
+		}
 		newContent=replaceAll(find,replace,newContent);
 	}
 	
