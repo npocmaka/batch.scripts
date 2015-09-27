1e8850da089cc7c88eeaf480ff2a0ca2d9b06bf2
Vasil Arnaudov <npocmaka@users.noreply.github.com>
2015-09-27 12:41:12 +0300

    Create md4.bat

---
+++ fileUtils/md4.bat
@@ -0,0 +1,56 @@
+@echo off
+setlocal enableDelayedExpansion
+if "%~1" equ "" (
+	echo no file passed
+	echo pass -help to see the help message
+	exit /b 1
+)
+
+for %%# in (-h -help /h /help) do (
+	if "%~1" equ "%%~#" (
+		echo generates MD4 checksum for a given file
+		(echo()
+		echo USAGE:
+		(echo()
+		echo %~nx0 file [variable]
+		(echo()
+		echo variable string in which the generated checksum will be stored
+		(echo()
+	)
+)
+
+if not exist "%~1" (
+	echo file %~1 does not exist
+	exit /b 2
+)
+
+if exist "%~1\" (
+	echo %~1 is a directory
+	exit /b 3
+)
+
+for %%# in (certutil.exe) do (
+	if not exist "%%~f$PATH:#" (
+		echo no certutil installed
+		echo for Windows XP professional and Windows 2003
+		echo you need Windows Server 2003 Administration Tools Pack
+		echo https://www.microsoft.com/en-us/download/details.aspx?id=3725
+		exit /b 4
+	)
+)
+
+set "MD4="
+for /f "skip=1 tokens=* delims=" %%# in ('certutil -hashfile "%~f1" MD4') do (
+	if not defined MD4 (
+		for %%Z in (%%#) do set "MD4=!MD4!%%Z"
+	)
+)
+
+if "%~2" neq "" (
+	endlocal && (
+		set "%~2=%MD4%"
+	) 
+) else (
+	echo %MD4%
+)
+endlocal
