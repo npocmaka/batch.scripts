586eed3c03afc5057d3edfba22be8a3a5aaac3c0
Vasil Arnaudov <npocmaka@users.noreply.github.com>
2015-10-21 12:54:41 +0300

    Create fileTImesJS.bat

---
+++ hybrids/jscript/fileTImesJS.bat
@@ -0,0 +1,52 @@
+@if (@X)==(@Y) @end /****** jscript comment ******
+@echo off
+setlocal
+for %%# in (/h /help -h -help) do (
+	if "%~1" equ "%%#" (
+		(echo()
+		echo %~nx0 - Displays LastModified,LastAccessed and DateCreated times of a given file
+		echo         in format YYYY-MM-DD hh:mm:ss , the milliseconds passed since 1970-1-1 , day of the week
+		(echo()
+		echo example:
+		echo call %~nx0 C:\file.ext
+	)
+)
+
+if "%~1" equ "" (
+	echo no file passed
+	exit /b 1
+)
+
+if not exist "%~f1" (
+	echo file "%~f1" does not exist
+	exit /b 2
+)
+
+if exist "%~f1\" (
+	echo "%~f1" is a directory but not a file
+)
+(echo()
+echo file timestamps for %~f1 :
+(echo()
+cscript //E:JScript //nologo "%~f0" "%~f1"
+exit /b %errorlevel%
+
+****** end of jscript comment ******/ 
+
+var file_loc = WScript.Arguments.Item(0);
+var fso = new ActiveXObject("Scripting.FileSystemObject");
+var the_file=fso.GetFile(file_loc);
+
+function printDateInfo(timeTag,date){
+	WScript.Echo( timeTag + " : " + date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate() + " " + date.getHours() + ":" +date.getMinutes() + ":" + date.getSeconds());
+	WScript.Echo(timeTag + " - milliseconds passed : " + date.getTime());
+	WScript.Echo( timeTag + " day of the week : " + date.getDay());
+	WScript.Echo();
+}
+var modified_date= new Date(the_file.DateLastModified);
+var creation_date= new Date(the_file.DateCreated);
+var accessed_date= new Date(the_file.DateLastAccessed);
+
+printDateInfo("Modified",modified_date);
+printDateInfo("Created",creation_date);
+printDateInfo("Accessed",accessed_date);
