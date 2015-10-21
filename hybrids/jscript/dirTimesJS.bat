afafcd64519920de5210b5b4d93365b72de9ce83
Vasil Arnaudov <npocmaka@users.noreply.github.com>
2015-10-21 13:55:27 +0300

    Create dirTimesJS.bat

---
+++ hybrids/jscript/dirTimesJS.bat
@@ -0,0 +1,53 @@
+@if (@X)==(@Y) @end /****** jscript comment ******
+@echo off
+setlocal
+for %%# in (/h /help -h -help) do (
+	if "%~1" equ "%%#" (
+		(echo()
+		echo %~nx0 - Displays LastModified,LastAccessed and DateCreated times of a given directory
+		echo         in format YYYY-MM-DD hh:mm:ss , the milliseconds passed since 1970-1-1 , day of the week
+		(echo()
+		echo example:
+		echo call %~nx0 "C:\directory"
+	)
+)
+
+if "%~1" equ "" (
+	echo no directory passed
+	exit /b 1
+)
+
+if not exist "%~f1" (
+	echo directory "%~f1" does not exist
+	exit /b 2
+)
+
+if exist "%~f1\"  if not exist "%~f1" (
+	echo "%~f1" is a file but not a directory
+	exit /b 3
+) 
+
+(echo()
+echo directory timestamps for %~f1 :
+(echo()
+cscript //E:JScript //nologo "%~f0" "%~f1"
+exit /b %errorlevel%
+
+****** end of jscript comment ******/ 
+
+var fso = new ActiveXObject("Scripting.FileSystemObject");
+var directory=fso.GetFolder(WScript.Arguments.Item(0));
+
+function printDateInfo(timeTag,date){
+	WScript.Echo( timeTag + " : " + date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate() + " " + date.getHours() + ":" +date.getMinutes() + ":" + date.getSeconds());
+	WScript.Echo(timeTag + " - milliseconds passed : " + date.getTime());
+	WScript.Echo( timeTag + " day of the week : " + date.getDay());
+	WScript.Echo();
+}
+var modified_date= new Date(directory.DateLastModified);
+var creation_date= new Date(directory.DateCreated);
+var accessed_date= new Date(directory.DateLastAccessed);
+
+printDateInfo("Modified",modified_date);
+printDateInfo("Created",creation_date);
+printDateInfo("Accessed",accessed_date);
