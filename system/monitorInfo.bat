3d4b737009d55958494c66815cba1f395ce6e84d
Vasil Arnaudov <npocmaka@users.noreply.github.com>
2016-01-21 00:02:58 +0200

    Update monitorInfo.bat

--- system/monitorInfo.bat
+++ system/monitorInfo.bat
@@ -1,8 +1,7 @@
 @echo off
 
 del ~.txt /q /f >nul 2>nul
-dxdiag /t ~
-w32tm /stripchart /computer:localhost /period:1 /dataonly /samples:3  >nul 2>&1
+start "" /w dxdiag /t ~
 setlocal enableDelayedExpansion
 set currmon=1 
 for /f "tokens=2 delims=:" %%a in ('find "Current Mode:" ~.txt') do (
