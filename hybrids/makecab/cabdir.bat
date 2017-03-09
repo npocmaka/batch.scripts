014e348d8832a7d8c710f63e8f0055d2f9a138b2
Vasil Arnaudov <npocmaka@users.noreply.github.com>
2017-03-09 12:05:45 +0200

    Update cabdir.bat

--- hybrids/makecab/cabdir.bat
+++ hybrids/makecab/cabdir.bat
@@ -1,19 +1,29 @@
 ;@echo off
 
 ;;;;; rem start of the batch part  ;;;;;
+;
+;for %%a in (/h /help -h -help) do ( 
+;	if /I "%~1" equ "%%~a" if "%~2" equ "" (
+;		echo compressing directory to cab file	
+;		echo Usage:
+;		echo(
+;		echo %~nx0 "directory" "cabfile"
+;		echo(
+;     	echo to uncompress use:
+;		echo EXPAND cabfile -F:* .
+;		echo(
+;		echo Example:
+;		echo(
+;		echo %~nx0 "c:\directory\logs" "logs"
+;		exit /b 0
+;	)
+; )
+;
 ; if "%~2" EQU "" (
 ;	echo invalid arguments.For help use:
 ;	echo %~nx0 /h
 ;   exit /b 1
 ;)
-;for %%a in (/h /help -h -help) do ( 
-;	if "%~1" equ "%%~a" (
-;		echo compressing directory to cab file	
-;		echo %~nx0 directory cabfile
-;     	echo to uncompress use:
-;		echo EXPAND cabfile -F:* .
-;	)
-; )
 ;
 ; set "dir_to_cab=%~f1"
 ;
@@ -37,17 +47,17 @@
 ;   set "destdir=%dir_name%!_dir:%path_to_dir%=!"
 ;	(echo(.Set DestinationDir=!destdir!>>"%tmp%\makecab.dir.ddf")
 ; 	for %%# in ("%%a\*") do (
-;		(echo("%%~s#"  /inf=no>>"%tmp%\makecab.dir.ddf")
+;		(echo("%%~f#"  /inf=no>>"%tmp%\makecab.dir.ddf")
 ;	)
 ;)
 ;(echo(.Set DestinationDir=!dir_name!>>"%tmp%\makecab.dir.ddf")
 ; 	for %%# in ("%~f1\*") do (
 ;		
-;		(echo("%%~s#"  /inf=no>>"%tmp%\makecab.dir.ddf")
+;		(echo("%%~f#"  /inf=no>>"%tmp%\makecab.dir.ddf")
 ;	)
 
 ;makecab /F "%~f0" /f "%tmp%\makecab.dir.ddf" /d DiskDirectory1=%cd% /d CabinetNameTemplate=%cab_file%.cab
-;del /q /f "%tmp%\makecab.dir.ddf"
+;rem del /q /f "%tmp%\makecab.dir.ddf"
 ;exit /b %errorlevel%
 
 ;;
