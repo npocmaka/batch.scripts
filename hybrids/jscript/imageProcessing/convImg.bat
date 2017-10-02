8cbcfb00cab93087a1c2e825539fa3e0e69bf826
Vasil Arnaudov <npocmaka@users.noreply.github.com>
2017-10-02 17:14:48 +0300

    Update convImg.bat

--- hybrids/jscript/imageProcessing/convImg.bat
+++ hybrids/jscript/imageProcessing/convImg.bat
@@ -14,16 +14,82 @@ https://msdn.microsoft.com/en-us/library/windows/desktop/ms630819(v=vs.85).aspx
 https://msdn.microsoft.com/en-us/library/windows/desktop/ms630829(v=vs.85).aspx
 */
 
+////////////////////////////
+////                      //
+/**/     var QUALITY=100; //
+////                      //
+////////////////////////////
+
+function printHelp(){
+	WScript.Echo( WScript.ScriptName + " converts image from one format to another.Supported formats are BPM,JPG,PNG,GIF,TIFF");
+	WScript.Echo( "");
+	WScript.Echo( WScript.ScriptName + " source.file target.format [force]");
+	WScript.Echo( "");
+	WScript.Echo("source.file path to the file to be converted");
+	WScript.Echo("target.format path to the fresult file.The extension of the file will be taken as the needed format in the result.");
+	WScript.Echo("force - if argument given the target file will be overwritten if exists.");
+}
+
+if (WScript.Arguments.Length==0){
+	printHelp();
+}
+
+if (WScript.Arguments.Length<2){
+	WScript.Echo("Invalid arguments");
+	printHelp();
+	Wscript.Quit(1);
+}
+
+var force=false;
 var source=WScript.Arguments.Item(0);
 var target=WScript.Arguments.Item(1);
+if(WScript.Arguments.Length==3){
+	force=true;
+}
+
 
 var targetFormat=target.split(".")[target.split(".").length-1].toUpperCase();
 
 var imageFile = new ActiveXObject("WIA.ImageFile"); 
-var imageProcess = new ActiveXObject("WIA.ImageProcess"); 
+var imageProcess = new ActiveXObject("WIA.ImageProcess");
+var fileSystem = new ActiveXObject("Scripting.FileSystemObject");
+
+function existsFile(path){
+	if (fileSystem.FileExists(path))
+		return true;
+}
+
+function existsFolder(path){
+	if (fileSystem.FolderExists(path))
+		return true;
+}
+
+function deleteFile(path){
+	fileSystem.DeleteFile(path);
+}
+
+
+if(!existsFile(source)){
+	WScript.Echo("Source image: " + source +" does not exists");
+	WScript.Quit(40);
+}
+
+if(existsFile(target) && !force){
+	WScript.Echo("Target image: " + target +" already exists");
+	WScript.Quit(45);
+}
+
+if(existsFolder(target)){
+	WScript.Echo("There's existing folder with the target file  (" + target +") name");
+	WScript.Quit(46);
+}
+
+if(existsFile(target) && force){
+	deleteFile(target);
+} 
 
 //WScript.Echo(imageProcess.FilterInfos.Count); 
-var count=imageProcess.FilterInfos.Count; 
+//var count=imageProcess.FilterInfos.Count; 
 
 
 
@@ -67,11 +133,16 @@ function convert(image,format){
 	
 	imageProcess.Filters.Add(imageProcess.FilterInfos("Convert").FilterID);
 	imageProcess.Filters(1).Properties("FormatID").Value = filterFormat;
-	imageProcess.Filters(1).Properties("Quality").Value = 100;
+	imageProcess.Filters(1).Properties("Quality").Value = QUALITY;
 	
 	return imageProcess.Apply(image);
 }
 
 loadImage(imageFile,source);
-var converted=convert(imageFile,targetFormat);
-converted.SaveFile(target);
+var sourceFormat=ID2Format(imageFile.FormatID);
+if(sourceFormat!== targetFormat) {
+	var converted=convert(imageFile,targetFormat);
+	converted.SaveFile(target);
+} else {
+	fileSystem.CopyFile(source,target,force); 
+}
