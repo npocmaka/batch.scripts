ec4fa08936d342b84709fbb40ae18a1e02cd2b96
Vasil Arnaudov <npocmaka@users.noreply.github.com>
2017-10-25 20:31:51 +0300

    Create isInteger.bat

---
+++ math/isInteger.bat
@@ -0,0 +1,18 @@
+@echo off
+
+:isInterer  input [returnVar]
+setlocal enableDelayedexpansion
+set "input=%~1"
+
+if "!input:~0,1!" equ "-" set "input=!input:~1!"
+
+for %%# in (1 2 3 4 5 6 7 8 9 0) do (
+    set "input=!input:%%#=!"
+)
+
+if "!input!" equ "" (set result=true
+) else (
+ set result=false
+)
+
+endlocal & if "%~2" neq "" (set %~2=%result%) else echo %result%
