55078e31719ab0af9a84944eb2db738ddcaaf140
Vasil Arnaudov <npocmaka@users.noreply.github.com>
2017-12-12 17:43:48 +0200

    Update endsWith.bat

--- stringutils/endsWith.bat
+++ stringutils/endsWith.bat
@@ -1,5 +1,5 @@
 :endsWith [%1 - string to be checked;%2 - string for checking ]
-::http://ss64.org/viewtopic.php?id=1687
+
 @echo off
 rem :: sets errorlevel to 1 if %1 ends with %2 else sets errorlevel to 0
 
@@ -14,19 +14,17 @@ set LF=^
 rem ** Two empty lines are required
 rem echo off
 for %%L in ("!LF!") DO (
- 	for /f "delims=" %%R in ("!checker!") do ( 
- 		rem set "var=!string:%%~R%%~R=%%~L!"
- 		set "var=!string:%%~R=%%L#!"
- 	)
+    for /f "delims=" %%R in ("!checker!") do ( 
+        rem set "var=!string:%%~R%%~R=%%~L!"
+        set "var=!string:%%~R=%%L#!"
+    )
 )
 for /f "delims=" %%P in (""!var!"") DO (
-	set "temp=%%~P"
+    set "temp=%%~P"
 )
-if "%temp%" EQU "#" goto :yes
-goto :no
-:yes
-endlocal & verify set_error 2>nul
-goto :eof
-:no
-endlocal & ( echo | shift )
-goto :eof
+
+if "%temp%" EQU "#" (
+    endlocal & exit /b 1
+) else (
+   endlocal & exist /b 0
+)
