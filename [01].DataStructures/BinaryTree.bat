875d08c41bb678d6365f598a1bb34903b85f5d8b
Sercan Sebetçi <86542016+sebetci@users.noreply.github.com>
2022-05-26 01:27:14 +0300

    add: added new parameters to follow in debug mode

--- [01].DataStructures/BinaryTree.bat
+++ [01].DataStructures/BinaryTree.bat
@@ -34,10 +34,10 @@ GOTO :EOF
    :: The following commands find for the value 1 in the tree.
    :: If the FFIND method finds the value 1, the variable %ERRORLEVEL% has the value 1.
    ECHO.
-   ECHO [VDebug:MAIN] Searching for value 1
-   CALL :FFIND VTreeName 1
+   ECHO [VDebug:MAIN] Searching for value 5
+   CALL :FFIND VTreeName 5
    ECHO [VDebug:MAIN] Return Value: %ERRORLEVEL%
-   IF %ERRORLEVEL% EQU 0 (ECHO [VDebug:MAIN] 1 was found in the three.) ELSE (ECHO [VDebug:MAIN] 1 not found in tree.)
+   IF %ERRORLEVEL% EQU 0 (ECHO [VDebug:MAIN] 5 was found in the three.) ELSE (ECHO [VDebug:MAIN] 5 not found in tree.)
    
    :: The following commands find for the value 1 in the tree.
    :: If the FFIND method does not find the value 8, the variable %ERRORLEVEL% will have the value 0.
@@ -62,12 +62,13 @@ GOTO :EOF
 :: @return     Returns 0 if value is found in the tree.
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :FFIND VTreeName VValue
-   SETLOCAL ENABLEDELAYEDEXPANSION
+   :: SETLOCAL ENABLEDELAYEDEXPANSION
    SET /A VValue=%~2
 
-   IF %VDebug% EQU 1 ECHO [VDebug:FIND] Value-1: %VValue%
-   IF %VDebug% EQU 1 ECHO [VDebug:FIND] Value-2: %2
-   IF %VDebug% EQU 1 ECHO [VDebug:FIND] Tree Name: %1
+   IF %VDebug% EQU 1 ECHO [VDebug:FIND] Content-1: %VTreeName%
+   IF %VDebug% EQU 1 ECHO [VDebug:FIND] Content-2: %VValue%
+   IF %VDebug% EQU 1 ECHO [VDebug:FIND] Param-1: %1
+   IF %VDebug% EQU 1 ECHO [VDebug:FIND] Param-2: %2
 
    IF %VTreeName% EQU %2 (
       ENDLOCAL & (
@@ -126,7 +127,7 @@ GOTO :EOF
          )
       ) ELSE (
          ENDLOCAL & (
-            CALL ::FINSERT %~1R %VValue%
+            CALL :FINSERT %~1R %VValue%
          )
       )
    )
@@ -139,7 +140,7 @@ GOTO :EOF
          )
          ) ELSE (
             ENDLOCAL & (
-               CALL ::FINSERT %~1R %VValue%
+               CALL :FINSERT %~1R %VValue%
             )
          )
    )
