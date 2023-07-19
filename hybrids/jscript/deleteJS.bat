08727d7f835d4be668dbe8adaaa7aff503028d87
Vasil Arnaudov <npocmaka@users.noreply.github.com>
2023-07-19 21:06:56 +0300

    Update deleteJS.bat

--- hybrids/jscript/deleteJS.bat
+++ hybrids/jscript/deleteJS.bat
@@ -13,7 +13,7 @@ FSOObj = new ActiveXObject("Scripting.FileSystemObject");
 var ARGS = WScript.Arguments;
 if (ARGS.Length < 1 ) {
  WScript.Echo("No file passed");
- WScript.Echo("Puts file or folder in recycle bin.");
+ WScript.Echo("Puts file or folder in the recycle bin.");
  WScript.Echo(WScript.ScriptName + " file|folder");
  WScript.Quit(1);
 }
